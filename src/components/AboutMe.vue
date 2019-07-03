46761774d63e94239acc167ab9594be8f8513363
sustly <279719732@qq.com>
2019-07-03 10:17:59 +0800

    show Article

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
     <app-header></app-header>
+    <h3 style="text-align: center">说明</h3>
+    <p style="padding-left: 20px">目前只有增删改查，全文检索功能，后续会增加其他功能。</p>
     <app-footer></app-footer>
   </div>
 
