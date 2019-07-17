297e9b4031108653b8515685c53f8be057590a00
sustly <279719732@qq.com>
2019-07-17 08:52:19 +0800

    说明

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <app-header></app-header>
     <h3 style="text-align: center">说明</h3>
-    <p style="padding-left: 20px">2019.7.3：目前只有增删改查，全文检索功能，后续会增加其他功能。</p>
     <p style="padding-left: 20px">2019.7.16：使用懒加载的方式优化第一次加载页面的时间长度。</p>
+    <p style="padding-left: 20px">2019.7.3：目前只有增删改查，全文检索功能，后续会增加其他功能。</p>
     <app-footer></app-footer>
   </div>
 
